fdc16e962568c221806d4b1aad7e057cdbeca731
Don Hinds <hindsdw@gmail.com>
2019-12-06 14:43:40 -0500

    having trouble packing a javascript file

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,7 +1,7 @@
 require("expose-loader?$!expose-loader?jQuery!jquery");
 require("bootstrap/dist/js/bootstrap.bundle.js");
 
-import drp from "tiny-date-picker/dist/date-range-picker.js";
 $(() => { 
 
 });
+
